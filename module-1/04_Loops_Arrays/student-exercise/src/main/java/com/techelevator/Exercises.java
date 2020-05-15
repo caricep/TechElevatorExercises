97ec77ce88c32923954de169201da1606051d8e0
Carice Poston <carice.cp@gmail.com>
2020-05-15 00:49:12 -0400

    Day 4 Q9

--- module-1/04_Loops_Arrays/student-exercise/src/main/java/com/techelevator/Exercises.java
+++ module-1/04_Loops_Arrays/student-exercise/src/main/java/com/techelevator/Exercises.java
@@ -99,6 +99,11 @@ public class Exercises {
 	 maxEnd3([2, 11, 3]) → [3, 3, 3]
 	 */
 	public int[] maxEnd3(int[] nums) {
+		if (nums[0] > nums[2]) {
+			return new int[] {nums [0], nums [0], nums [0]};
+		} else if (nums[2] > nums[0]) {
+			return new int[] {nums[2], nums[2], nums[2]};
+		}
 		return new int[] {};
 	}
 
