64e725b7f19c27a2e514ab7b08f1e281bf4a7019
Carice Poston <carice.cp@gmail.com>
2020-05-16 17:41:43 -0400

    Day 5 Fibonacci

--- module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -1,9 +1,34 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
 
+        System.out.print("Please enter the Fibonacci number: ");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        int  fibonacciNumberZeroOrOne = Integer.parseInt(scanner.nextLine());
+        	if (fibonacciNumberZeroOrOne == 0)
+        System.out.println(fibonacciNumberZeroOrOne);
+        	else if (fibonacciNumberZeroOrOne == 1)
+        	System.out.println("0 " + fibonacciNumberZeroOrOne);
+  
+	}
+        public int fibonacciNumberSpecial(int[] nums) {
+        	int arrayFibonacciSpecial = 0;
+        	
+        	for (int i = 0; i < nums.length; i++)
+        		if (nums[0] == 0) {
+        			System.out.println(arrayFibonacciSpecial);
+        	}
+        	
+    		
+		return 0;
+		
+		
 	}
 
 }
