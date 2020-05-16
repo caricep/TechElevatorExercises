20313b8ebbab03515041098cddfbaa9b7ce720c2
Carice Poston <carice.cp@gmail.com>
2020-05-16 15:28:19 -0400

    Day 4 Q13 check

--- module-1/04_Loops_Arrays/student-exercise/src/main/java/com/techelevator/Exercises.java
+++ module-1/04_Loops_Arrays/student-exercise/src/main/java/com/techelevator/Exercises.java
@@ -184,6 +184,11 @@ public class Exercises {
 	 has22([2, 1, 2]) → false
 	 */
 	public boolean has22(int[] nums) {
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] == 2 && nums[i + 1] == 2) 			
+				return true;
+		}
+		
 		return false;
 	}
 	
