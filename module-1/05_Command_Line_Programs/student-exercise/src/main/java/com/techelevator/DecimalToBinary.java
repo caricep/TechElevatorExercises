6415e6af611d5875d293f97ffb0c98269156f9a1
Carice Poston <carice.cp@gmail.com>
2020-05-17 14:21:21 -0400

    Decimal to Binary Save

--- module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,9 +1,24 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
 
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Please enter in a series of decimal values (separated by spaces): ");
+		
+		for ( ; in.hasNextInt() ; ) {
+			
+			int decimalValue = in.nextInt();
+			
+			double binaryValue = (int) decimalValue * 0.378;
+			
+			
+			System.out.printf("\n%s in binary is %s ", decimalValue, (int) binaryValue);
+		}
 	}
 
 }
