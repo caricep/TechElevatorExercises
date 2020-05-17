d5bb77cba3d4dcd2d82662637f4583a7497980db
Carice Poston <carice.cp@gmail.com>
2020-05-16 21:54:24 -0400

    Day 5 TempConvert

--- module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/TempConvert.java
@@ -6,19 +6,36 @@ public class TempConvert {
 
 	public static void main(String[] args) {
 		
+	
+		System.out.print("Please enter the temperature: ");
 		Scanner in = new Scanner (System.in);
 		
-		System.out.print("Please enter the temperature: ");
-	
 		int tempInput = in.nextInt();
 		in.nextLine();
 		
-		System.out.print("Is the temperature in Celsius (0), or Fahrenheit (1)? ");
+		System.out.print("Is the temperature in (C)elsius, or (F)ahrenheit? ");
 		
-		String tempFC = in.nextLine();
+		String[] tempFC = {"C", "F"};
+		String userInput = in.nextLine();
+		int tempFahrenheit = Integer.parseInt(userInput);
+		int tempCelsius = Integer.parseInt(userInput);
+		
+		System.out.printf( "%sF, is %sC.", doConversion(tempInput, userInput)); }
+		
+		public static int doConversion(int tempInput, String userInput) {
+			
+			double tempFahrenheit = (int) tempInput * 1.8 + 32; 
+			double tempCelsius = (int) (tempInput - 32) / 1.8;
+			
+			if (userInput == "F") {
+				return (int) tempFahrenheit;
+			} else if (userInput == "C") {
+				return (int) tempCelsius;
+			}
+			
+			return 0;
 		
 		
-	//	System.out.printf( "%sF, is %sC.", tempFahrenheit, tempCelsius);
 		
 	}
 
