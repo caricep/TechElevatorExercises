d102b75ec044728ce6f1af72f5cd2974ea6109b7
Carice Poston <carice.cp@gmail.com>
2020-05-17 14:12:05 -0400

    Decimal to Binary Backup

--- module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/LinearConvert.java
@@ -1,9 +1,33 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class LinearConvert {
 
 	public static void main(String[] args) {
-
+		
+		// Foot to Meter conversion: m = f * 0.3048
+		// Meter to Foot conversion: f = m * 3.2808399
+		
+		System.out.print("Please enter the length: ");
+		
+		Scanner in = new Scanner (System.in);
+		
+		int lengthInput = in.nextInt();
+		in.nextLine();
+		
+		System.out.print("Is the measurement in (m)eter, or (f)eet? ");
+		
+		String lengthMetersOrFeet = in.nextLine();
+				
+		if (lengthMetersOrFeet.equals("m")) {
+			int lengthFeet = (int) (lengthInput * 3.2808399);
+			System.out.println(lengthInput + "m is " + lengthFeet + "f.");
+		}
+		else if (lengthMetersOrFeet.equals("f")) {
+			int lengthMeters = (int) (lengthInput * 0.3048);
+			System.out.println(lengthInput + "f is " + lengthMeters + "m.");
+		}
 	}
 
 }
--- module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/TempConvert.java
@@ -6,8 +6,11 @@ public class TempConvert {
 
 	public static void main(String[] args) {
 		
+		// Fahrenheit to Celsius conversion:  Tc = (Tf - 32) / 1.8
+		// Celsius to Fahrenheit conversion:  Tf = Tc * 1.8 + 32
 	
 		System.out.print("Please enter the temperature: ");
+		
 		Scanner in = new Scanner (System.in);
 		
 		int tempInput = in.nextInt();
@@ -15,27 +18,18 @@ public class TempConvert {
 		
 		System.out.print("Is the temperature in (C)elsius, or (F)ahrenheit? ");
 		
-		String[] tempFC = {"C", "F"};
-		String userInput = in.nextLine();
-		int tempFahrenheit = Integer.parseInt(userInput);
-		int tempCelsius = Integer.parseInt(userInput);
-		
-		System.out.printf( "%sF, is %sC.", doConversion(tempInput, userInput)); }
-		
-		public static int doConversion(int tempInput, String userInput) {
+		String tempFC = in.nextLine();
+				
+		if (tempFC.equals("C")) {
+			int tempFahrenheit = (int) (tempInput * 1.8 + 32);
+			System.out.println(tempInput + "C is " + tempFahrenheit + "F.");
+		}
+		else if (tempFC.equals("F")) {
+			int tempCelcius = (int) ((tempInput - 32) / 1.8);
+			System.out.println(tempInput + "F is " + tempCelcius + "C.");
+		}
 			
-			double tempFahrenheit = (int) tempInput * 1.8 + 32; 
-			double tempCelsius = (int) (tempInput - 32) / 1.8;
-			
-			if (userInput == "F") {
-				return (int) tempFahrenheit;
-			} else if (userInput == "C") {
-				return (int) tempCelsius;
-			}
-			
-			return 0;
-		
-		
+	
 		
 	}
 
