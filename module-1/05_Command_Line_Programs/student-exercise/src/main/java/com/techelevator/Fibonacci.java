b91b8827f80cafe0d64d2bed301cd216c1c46f3c
Carice Poston <carice.cp@gmail.com>
2020-05-17 12:07:43 -0400

    Fibonacci 0 and 1 done

--- module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -6,27 +6,33 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 		
 		System.out.print("Please enter the Fibonacci number: ");
-			
-        int  fibonacciNumber = Integer.parseInt(scanner.nextLine());
-        	if (fibonacciNumber == 0) {
-        		System.out.println(fibonacciNumber);
-        	}
-        	else if (fibonacciNumber == 1)
-        		System.out.println("0, " + fibonacciNumber);
-	
-        	int enteredNumber = 0;
-        	int i;
+		
+		String userInput = in.nextLine();
+		int enteredNumber = Integer.parseInt(userInput);
+		
+		int[] fibonacciFirstThree = {0, 1, 1};
+		if (enteredNumber == 0) {
+			System.out.printf("Fibonacci sequence for 0 not found.");
+		} else if (enteredNumber == 1) {
+			System.out.println(fibonacciFirstThree[0] + ", " + fibonacciFirstThree[1]);
+		}
+		
+		int[] allFibonacciNumbers = {};
+		for (i = 2, i > 1, i++) {
+			if 
+		}
+		
         	
         	 {
         		
-        		 for (i = 2; i > 1; i++) {
-        			 if (fibonacciNumber > 1)	
-        			 i++;
-        				System.out.print("0, " + "1, " + "1, " + fibonacciNumber);
-        		}
+        	//	 for (i = 2; i > 1; i++) {
+        	//		 if (fibonacciNumber > 1)	
+        	//		 i++;
+        	//			System.out.print("0, " + "1, " + "1, " + fibonacciNumber);
+        		
         		
         			
         	}
