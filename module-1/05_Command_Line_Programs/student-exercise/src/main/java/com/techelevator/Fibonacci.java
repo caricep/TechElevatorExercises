3463c70c426ffa60a0216787307d96b58300fdee
Carice Poston <carice.cp@gmail.com>
2020-05-16 20:37:11 -0400

    Fibonacci check

--- module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -6,29 +6,48 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 
-        System.out.print("Please enter the Fibonacci number: ");
-        
-        Scanner scanner = new Scanner(System.in);
-        
-        int  fibonacciNumberZeroOrOne = Integer.parseInt(scanner.nextLine());
-        	if (fibonacciNumberZeroOrOne == 0)
-        System.out.println(fibonacciNumberZeroOrOne);
-        	else if (fibonacciNumberZeroOrOne == 1)
-        	System.out.println("0 " + fibonacciNumberZeroOrOne);
-  
-	}
-        public int fibonacciNumberSpecial(int[] nums) {
-        	int arrayFibonacciSpecial = 0;
-        	
-        	for (int i = 0; i < nums.length; i++)
-        		if (nums[0] == 0) {
-        			System.out.println(arrayFibonacciSpecial);
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Please enter the Fibonacci number: ");
+			
+        int  fibonacciNumber = Integer.parseInt(scanner.nextLine());
+        	if (fibonacciNumber == 0) {
+        		System.out.println(fibonacciNumber);
         	}
+        	else if (fibonacciNumber == 1)
+        		System.out.println("0, " + fibonacciNumber);
+	
+        	int enteredNumber = 0;
+        	int i;
+        	
+        	 {
+        		
+        		 for (i = 2; i > 1; i++) {
+        			 if (fibonacciNumber > 1)	
+        			 i++;
+        				System.out.print("0, " + "1, " + "1, " + fibonacciNumber);
+        		}
+        		
+        			
+        	}
+        		
+	
+	
+	
+			
+        	{
+        			
+        			}
+        	        
+        				
+        					
+        				
+  
+        			
+        
+        		
         	
     		
-		return 0;
-		
-		
-	}
+        	}
 
 }
