2496b025c52f8895c9a05e2fddb6bca982c99c23
Brian <brian@techelevator.com>
2020-05-18 15:29:46 -0400

    Lecture code

--- module-1/05_Command_Line_Programs/lecture-final/src/main/java/com/techelevator/EarthToSunWeightConverter.java
+++ module-1/05_Command_Line_Programs/lecture-final/src/main/java/com/techelevator/EarthToSunWeightConverter.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class EarthToSunWeightConverter {
 
 	/*
@@ -13,8 +15,23 @@ public class EarthToSunWeightConverter {
 	public static void main(String[] args) {
 		
 		int sunGravity = 28;
+		
+		System.out.print("Enter the Earth weight >>>");
+		
+		Scanner in = new Scanner( System.in );
+		
+		String userInput = in.nextLine();
+		
+		int earthWeight = Integer.parseInt(userInput);
 
-
+		int sunWeight = earthWeight * sunGravity;
+		
+		System.out.println();
+		
+		System.out.printf("%-15s    %-15s", "Earth (lbs)", "Sun (lbs)");
+		System.out.printf("%n----------------------------%n");
+		System.out.printf("%-15s    %-15s", earthWeight, sunWeight);
+		
 	}
 
 }
--- module-1/05_Command_Line_Programs/lecture-final/src/main/java/com/techelevator/RockPaperScissors.java
+++ module-1/05_Command_Line_Programs/lecture-final/src/main/java/com/techelevator/RockPaperScissors.java
@@ -12,7 +12,38 @@ public class RockPaperScissors {
 	 */
 	public static void main(String[] args) {
 		
+		String[] choices = { "Rock", "Paper", "Scissors" };
+		
 		Scanner in = new Scanner(System.in);
+		
+		for ( ; true ; ) {
+			System.out.println("Rock (0), Paper (1), or Scissors (2)");
+			String userInput = in.nextLine();
+			int userChoice = Integer.parseInt(userInput);
+			
+			int computerChoice = (int) (Math.random() * (2));
+	
+			System.out.println("You choose " + choices[userChoice] 
+					+ " and the computer choose " + choices[computerChoice]);
+			
+			if (computerChoice == userChoice) {
+				System.out.println("Tie!");
+			} else if ( (userChoice == 0 && computerChoice == 2) ||
+						(userChoice == 1 && computerChoice == 0) ||
+						(userChoice == 2 && computerChoice == 1)) {  // Player can Win
+				System.out.println("You won!");
+			} else {
+				System.out.println("The computer won!");
+			}
+			
+			System.out.println("Do you want to play again?  Yes (0) or No (1)");
+			int playAgain = in.nextInt();
+			in.nextLine();
+			if (playAgain == 1) {
+				break;
+			}
+		}
+			
 		
 	}
 }
