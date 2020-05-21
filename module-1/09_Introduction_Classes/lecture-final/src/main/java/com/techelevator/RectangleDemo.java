ee71dd13587b03bf29cce13def14343f5e9ff95c
Brian <brian@techelevator.com>
2020-05-21 12:48:33 -0400

    Lecture code

---
+++ module-1/09_Introduction_Classes/lecture-final/src/main/java/com/techelevator/RectangleDemo.java
@@ -0,0 +1,43 @@
+package com.techelevator;
+
+public class RectangleDemo {
+
+	public static void main(String[] args) {
+		
+		Rectangle wall = new Rectangle(10, 20);
+		Rectangle wall2 = new Rectangle(-12, 8);
+		
+		Rectangle wall3 = new Rectangle();
+		
+//		wall.setWidth( 10 );
+//		wall.setHeight( 20 );
+//		
+//		wall2.setWidth( 12 );
+//		wall2.setHeight( 8 );
+		
+		wall.setWidth( 219 );
+		
+
+		int wallWidth = wall.getWidth();
+		int wall2Height = wall2.getHeight();
+	
+		wall.setHeight( -10 );
+		
+		boolean isWall2Larger = wall2.isLargerThan( wall );
+		int wallArea = wall.getArea();
+		
+		int height = 22;
+		int width = 33;
+		
+		boolean isDimensionsLarger = wall.isLargerThan(height, width);
+		
+		System.out.println( wall );
+		
+		Rectangle wall4 = new Rectangle(10, 20);
+		Rectangle wall5 = new Rectangle(10, 20);
+		
+		boolean isSameWall = wall4.equals(wall5);
+		
+	}
+
+}
