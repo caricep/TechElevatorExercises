5493aca826708f0b9d277f054b9c10ac274c097a
Carice Poston <carice.cp@gmail.com>
2020-05-25 13:13:04 -0400

    Day 10 getters & setters

--- module-1/10_Encapsulation/student-exercise/src/main/java/com/techelevator/HomeworkAssignment.java
+++ module-1/10_Encapsulation/student-exercise/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -2,4 +2,36 @@ package com.techelevator;
 
 public class HomeworkAssignment {
 
+	private int totalMarks;
+	private int possibleMarks;
+	private String submitterName;
+	private String letterGrade;
+	
+	
+	
+	
+	
+	public int getTotalMarks() {
+		return totalMarks;
+	}
+	public void setTotalMarks(int totalMarks) {
+		this.totalMarks = totalMarks;
+	}
+	public String getSubmitterName() {
+		return submitterName;
+	}
+	public void setSubmitterName(String submitterName) {
+		this.submitterName = submitterName;
+	}
+	public int getPossibleMarks() {
+		return possibleMarks;
+	}
+	public String getLetterGrade() {
+		return letterGrade;
+	}
+	
+	
+	
+	
+	
 }
