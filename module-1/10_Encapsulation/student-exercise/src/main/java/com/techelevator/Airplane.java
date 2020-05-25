b6ee25589e62c56859094bf2b405ff37ba8e363d
Carice Poston <carice.cp@gmail.com>
2020-05-25 16:46:40 -0400

    Day 10 Encapsulation Complete

--- module-1/10_Encapsulation/student-exercise/src/main/java/com/techelevator/Airplane.java
+++ module-1/10_Encapsulation/student-exercise/src/main/java/com/techelevator/Airplane.java
@@ -2,4 +2,68 @@ package com.techelevator;
 
 public class Airplane {
 
+	private String planeNumber;
+	private int bookedFirstClassSeats = 0;
+	private int totalFirstClassSeats;
+	private int bookedCoachSeats = 0;
+	private int totalCoachSeats;
+
+	public Airplane(String planeNumber, int totalFirstClassSeats, int totalCoachSeats) {
+		this.planeNumber = planeNumber;
+		this.totalFirstClassSeats = totalFirstClassSeats;
+		this.totalCoachSeats = totalCoachSeats;
+	}
+
+	public boolean reserveSeats(boolean forFirstClass, int totalNumberOfSeats) {
+		if (forFirstClass) {
+			if (totalNumberOfSeats > getAvailableFirstClassSeats()) {
+				return false;
+			}
+				bookedFirstClassSeats = totalNumberOfSeats + bookedFirstClassSeats;
+				
+			} else {
+				if (totalNumberOfSeats > getAvailableCoachSeats()) {
+				return false;
+			}
+				bookedCoachSeats = totalNumberOfSeats + bookedCoachSeats;
+			} 
+			
+			return true;
+
+	}
+
+
+	public String getPlaneNumber() {
+		return planeNumber;
+	}
+
+	public int getBookedFirstClassSeats() {
+		return bookedFirstClassSeats;
+	}
+
+	public int getTotalFirstClassSeats() {
+		return totalFirstClassSeats;
+	}
+
+	public int getBookedCoachSeats() {
+		return bookedCoachSeats;
+	}
+
+	public int getTotalCoachSeats() {
+		return totalCoachSeats;
+	}
+	
+	public int getAvailableFirstClassSeats() {
+		if (totalFirstClassSeats - bookedFirstClassSeats > 0) {
+		return totalFirstClassSeats - bookedFirstClassSeats;
+		}
+	 return 0;
+	}
+	
+	public int getAvailableCoachSeats() {
+		if (totalCoachSeats - bookedCoachSeats > 0) {
+		return totalCoachSeats - bookedCoachSeats;
+		}
+	 return 0;
+	}
 }
--- module-1/10_Encapsulation/student-exercise/src/main/java/com/techelevator/Elevator.java
+++ module-1/10_Encapsulation/student-exercise/src/main/java/com/techelevator/Elevator.java
@@ -2,4 +2,49 @@ package com.techelevator;
 
 public class Elevator {
 
+	private int currentFloor = 1;
+	private int numberOfFloors;
+	private boolean doorOpen;
+	
+	
+	public Elevator(int totalNumberOffloors) {
+		this.numberOfFloors = totalNumberOffloors;
+	}
+	
+	public void openDoor() {
+		doorOpen = true;
+	}
+	
+	public void closeDoor() {
+		doorOpen = false;
+	}
+	
+	public void goUp(int desiredFloor) {
+		if (!doorOpen && 
+				desiredFloor <= numberOfFloors &&
+				desiredFloor > currentFloor) {
+			currentFloor = desiredFloor;
+		}
+	}
+	
+	public void goDown(int desiredFloor) {
+		if (!doorOpen &&
+				desiredFloor >= 1 &&
+				desiredFloor < currentFloor) {
+			currentFloor = desiredFloor;
+		}
+	}
+	
+	
+	public int getCurrentFloor() {
+		return currentFloor;
+	}
+	public int getNumberOfFloors() {
+		return numberOfFloors;
+	}
+	public boolean isDoorOpen() {
+		return doorOpen;
+	}
+	
+	
 }
