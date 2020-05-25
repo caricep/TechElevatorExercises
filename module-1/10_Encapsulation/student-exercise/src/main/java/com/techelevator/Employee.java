f2a13554cc22d7153e38498b9609309c8f0efe7c
Carice Poston <carice.cp@gmail.com>
2020-05-25 14:59:51 -0400

    Day 10 Employee

--- module-1/10_Encapsulation/student-exercise/src/main/java/com/techelevator/Employee.java
+++ module-1/10_Encapsulation/student-exercise/src/main/java/com/techelevator/Employee.java
@@ -2,4 +2,54 @@ package com.techelevator;
 
 public class Employee {
 
+	private int employeeId;
+	private String firstName;
+	private String lastName;
+	private String department;
+	private double annualSalary;
+
+	
+	public Employee(int employeeId, String firstName, String lastName, double salary) {
+		
+	}
+	
+	public void raiseSalary(double percent) {
+		
+	}
+	
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public double getAnnualSalary() {
+		return annualSalary;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+	
+	
+	
 }
