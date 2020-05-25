a8413b7a55db45933a45a317a7230008cf8aacf3
Carice Poston <carice.cp@gmail.com>
2020-05-25 13:46:44 -0400

    Day 10 FruitTree

--- module-1/10_Encapsulation/student-exercise/src/main/java/com/techelevator/FruitTree.java
+++ module-1/10_Encapsulation/student-exercise/src/main/java/com/techelevator/FruitTree.java
@@ -2,4 +2,27 @@ package com.techelevator;
 
 public class FruitTree {
 
+	private String typeOfFruit;
+	private int piecesOfFruitLeft;
+	
+	
+	public FruitTree(String typeOfFruit, int startingPiecesOfFruit) {
+		this.typeOfFruit = typeOfFruit;
+	}
+	
+	
+	public boolean pickFruit(int numberOfPiecesToRemove) {
+		
+		return true;
+	}
+	
+	
+	public String getTypeOfFruit() {
+		return typeOfFruit;
+	}
+	public int getPiecesOfFruitLeft() {
+		return piecesOfFruitLeft;
+	}
+	
+	
 }
