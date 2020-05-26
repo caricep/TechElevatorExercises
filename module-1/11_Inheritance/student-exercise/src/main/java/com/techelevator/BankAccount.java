517c7a4e758264fad6400625c338c2591d681677
Carice Poston <carice.cp@gmail.com>
2020-05-26 15:48:50 -0400

    Day 11 Inheritance BankAccount Backup

---
+++ module-1/11_Inheritance/student-exercise/src/main/java/com/techelevator/BankAccount.java
@@ -0,0 +1,20 @@
+package com.techelevator;
+
+public class BankAccount {
+
+	private String accountHolderName;
+	private String accountNumber;
+	private int balance = 0;
+	
+	public BankAccount(String accountHolderName, String accountNumber) {
+		
+	}
+	
+	public BankAccount(String accountHolderName, String accountNumber, int balance) {
+		
+	}
+	
+	
+	
+	
+}
