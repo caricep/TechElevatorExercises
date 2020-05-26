4b8e236d6914f42cb35e903ca95711d9c949bba0
Carice Poston <carice.cp@gmail.com>
2020-05-26 15:57:07 -0400

    Day 11 BankAccount Class

--- module-1/11_Inheritance/student-exercise/src/main/java/com/techelevator/BankAccount.java
+++ module-1/11_Inheritance/student-exercise/src/main/java/com/techelevator/BankAccount.java
@@ -4,17 +4,42 @@ public class BankAccount {
 
 	private String accountHolderName;
 	private String accountNumber;
-	private int balance = 0;
+	private int balance;
+	
 	
 	public BankAccount(String accountHolderName, String accountNumber) {
-		
+		this.accountHolderName = accountHolderName;
+		this.accountNumber = accountNumber;
 	}
 	
 	public BankAccount(String accountHolderName, String accountNumber, int balance) {
-		
+		this.accountHolderName = accountHolderName;
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+	}
+
+	
+	public int deposit(int amountToDeposit) {
+		this.balance = amountToDeposit + balance;
+		return balance;
 	}
 	
+	public int withdraw(int amountToWithdraw) {
+		this.balance = balance - amountToWithdraw;
+		return balance;
+	}
 	
+	public String getAccountHolderName() {
+		return accountHolderName;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
 	
 	
 }
