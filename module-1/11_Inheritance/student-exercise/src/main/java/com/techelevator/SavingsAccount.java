1fdb9e43b99817fff42097abe81f6972f41b3b03
Carice Poston <carice.cp@gmail.com>
2020-05-26 18:04:56 -0400

    Day 11 SavingsAccount Edits

--- module-1/11_Inheritance/student-exercise/src/main/java/com/techelevator/SavingsAccount.java
+++ module-1/11_Inheritance/student-exercise/src/main/java/com/techelevator/SavingsAccount.java
@@ -14,11 +14,14 @@ public class SavingsAccount extends BankAccount {
 	
 	@Override
 	public int withdraw(int amountToWithdraw) {
-		if (super.getBalance() < 150 && super.getBalance() >= 0) {
-			super.withdraw(amountToWithdraw + 2);
+		if (amountToWithdraw > getBalance()) {
+			super.withdraw(0);
 			
 		} else if (super.getBalance() > 150) {
 			super.withdraw(amountToWithdraw);
+			
+		} else if (super.getBalance() < 150 && super.getBalance() >= 0) {
+			super.withdraw(amountToWithdraw + 2);
 			
 		} 
 		return super.getBalance();
