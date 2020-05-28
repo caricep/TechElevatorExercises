70173e4fedb16d86e9f4f175ac296f18beec4ad8
Carice Poston <carice.cp@gmail.com>
2020-05-28 19:48:08 -0400

    Day 13 RectangleWall

---
+++ module-1/13_Inheritance_Abstract_Classes/student-exercise/src/main/java/com/techelevator/RectangleWall.java
@@ -0,0 +1,42 @@
+package com.techelevator;
+
+public class RectangleWall extends Wall {
+
+	private int length;
+	private int height;
+	
+	
+	public RectangleWall(String name, String color, int length, int height) {
+		super(name, color);
+		this.length = length;
+		this.height = height;
+	}
+
+	@Override
+	public String toString() {
+		return super.getName() + " (" + this.length + "x" + this.height + ") rectangle";
+	}
+	
+	public int getLength() {
+		return length;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	@Override
+	public int getArea() {
+		return length * height;
+	}
+
+	
+}
