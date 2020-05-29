57381990922a006acbe1501a433fb535ef61537d
Carice Poston <carice.cp@gmail.com>
2020-05-29 14:04:46 -0400

    Day 14 Unit Testing

---
+++ module-1/14_Unit_Testing/student-exercise/src/test/java/com/techelevator/AnimalGroupNameTest.java
@@ -0,0 +1,14 @@
+package com.techelevator;
+
+import org.junit.*;
+
+public class AnimalGroupNameTest {
+
+	private AnimalGroupName animalGroupName;
+	
+	@Before
+	public void createAnimalGroup() {
+		animalGroupName = new AnimalGroupName();
+	}
+	
+}
