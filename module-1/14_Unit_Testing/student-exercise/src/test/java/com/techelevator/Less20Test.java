907ae8ff064d378a956d2ff17d20f5fcf0f58875
Carice Poston <carice.cp@gmail.com>
2020-06-01 18:26:40 -0400

    Unit Testing Updates Almost Done

--- module-1/14_Unit_Testing/student-exercise/src/test/java/com/techelevator/Less20Test.java
+++ module-1/14_Unit_Testing/student-exercise/src/test/java/com/techelevator/Less20Test.java
@@ -1,7 +1,6 @@
 package com.techelevator;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 public class Less20Test {
 
@@ -13,7 +12,46 @@ private Less20 less20;
 	}	
 	
 	
+	@Test
+	public void multiple_of_20_is_false() {
+		boolean multipleNumber = less20.isLessThanMultipleOf20(10);
+		
+		Assert.assertFalse(multipleNumber);
+	}
 	
+	@Test
+	public void multiple_of_19_is_true() {
+		boolean multipleNumber = less20.isLessThanMultipleOf20(19);
+		
+		Assert.assertTrue(multipleNumber);
+	}
 	
+	@Test
+	public void multiple_of_18_is_true() {
+		boolean multipleNumber = less20.isLessThanMultipleOf20(18);
+		
+		Assert.assertTrue(multipleNumber);
+	}
+	
+	@Test
+	public void multiple_is_0_is_false() {
+		boolean multipleNumber = less20.isLessThanMultipleOf20(0);
+		
+		Assert.assertFalse(multipleNumber);
+	}
+	
+	@Test
+	public void negative_multiple_of_19_is_false() {
+		boolean multipleNumber = less20.isLessThanMultipleOf20(-19);
+		
+		Assert.assertFalse(multipleNumber);
+	}
+	
+	@Test
+	public void negative_multiple_of_18_is_false() {
+		boolean multipleNumber = less20.isLessThanMultipleOf20(-18);
+		
+		Assert.assertFalse(multipleNumber);
+	}
 	
 }
