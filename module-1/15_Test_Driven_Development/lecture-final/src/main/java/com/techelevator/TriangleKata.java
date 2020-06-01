27298cc91aefc624c4edc2975de54a4f13ec6b1e
Brian <brian@techelevator.com>
2020-06-01 11:11:14 -0400

    extracted invalid logic

--- module-1/15_Test_Driven_Development/lecture-final/src/main/java/com/techelevator/TriangleKata.java
+++ module-1/15_Test_Driven_Development/lecture-final/src/main/java/com/techelevator/TriangleKata.java
@@ -4,9 +4,8 @@ public class TriangleKata {
 
 	public String classify(int side1, int side2, int side3) {
 		
-		if (side1 + side2 <= side3 ||
-				side2 + side3 <= side1 ||
-				side1 + side3 <= side2) {
+		
+		if (!isValidTriangle(side1, side2, side3)) {
 			return "invalid";
 		}
 	
@@ -22,6 +21,19 @@ public class TriangleKata {
 		
 		
 		return "scalene";
+	}
+	
+	private boolean isValidTriangle(int side1, int side2, int side3) {
+		int sumOf1and2 = side1 + side2;
+		int sumOf2and3 = side2 + side3;
+		int sumOf1and3 = side1 + side3;
+		
+		if (sumOf1and2 <= side3 ||
+				sumOf2and3 <= side1 ||
+				sumOf1and3 <= side2) {
+			return false;
+		}
+		return true;
 	}
 	
 }
