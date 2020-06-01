1257be8b440af28dd428888abfaabe9076457b71
Carice Poston <carice.cp@gmail.com>
2020-06-01 19:49:46 -0400

    Day 14 Unit Testing Complete

--- module-1/14_Unit_Testing/student-exercise/src/test/java/com/techelevator/StringBitsTest.java
+++ module-1/14_Unit_Testing/student-exercise/src/test/java/com/techelevator/StringBitsTest.java
@@ -1,5 +1,29 @@
 package com.techelevator;
 
+import org.junit.*;
+
 public class StringBitsTest {
 
+	private StringBits stringBits;
+	
+	@Before
+	public void setup() {
+		stringBits = new StringBits();
+	}
+	
+	@Test
+	public void string_is_empty_returns_blank() {
+		String everyOtherLetter = stringBits.getBits("");
+		
+		Assert.assertEquals("", everyOtherLetter);
+	}
+	
+	@Test
+	public void string_returns_every_other_letter_starting_with_first_char() {
+		String everyOtherLetter = stringBits.getBits("Pineapple");
+		
+		Assert.assertEquals("Pnape", everyOtherLetter);
+	}
+	
+	
 }
