b35acdd7abee7e8b57bb3e9b5e0cb68872de4e6b
Brian <brian@techelevator.com>
2020-06-01 10:30:09 -0400

    Passed equilateral test

--- module-1/15_Test_Driven_Development/lecture-final/src/main/java/com/techelevator/TriangleKata.java
+++ module-1/15_Test_Driven_Development/lecture-final/src/main/java/com/techelevator/TriangleKata.java
@@ -4,6 +4,10 @@ public class TriangleKata {
 
 	public String classify(int side1, int side2, int side3) {
 		
+		if( side1 == side2 && side1 == side3) {
+			return "equilateral";
+		}
+		
 		
 		return null;
 	}
