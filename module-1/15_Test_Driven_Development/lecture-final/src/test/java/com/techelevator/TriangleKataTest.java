506cf6660248a3b2617b789dc59e28d4e57b0644
Brian <brian@techelevator.com>
2020-06-01 10:39:01 -0400

    Added invalid for sides 2 and 3 < 1

--- module-1/15_Test_Driven_Development/lecture-final/src/test/java/com/techelevator/TriangleKataTest.java
+++ module-1/15_Test_Driven_Development/lecture-final/src/test/java/com/techelevator/TriangleKataTest.java
@@ -17,8 +17,15 @@ public class TriangleKataTest {
 	}
 	
 	@Test
-	public void identifies_invalid_triangle_with_positive_sides() {
+	public void identifies_invalid_triangle_with_1_and_2_lessThan_3() {
 		Assert.assertEquals("invalid", triangleKata.classify(6, 6, 13));
+		Assert.assertEquals("invalid", triangleKata.classify(6, 6, 12));
+	}
+	
+	@Test
+	public void identifies_invalid_triangle_with_2_and_3_lessThan_1() {
+		Assert.assertEquals("invalid", triangleKata.classify(13, 6, 6));
+		Assert.assertEquals("invalid", triangleKata.classify(12, 6, 6));
 	}
 	
 }
