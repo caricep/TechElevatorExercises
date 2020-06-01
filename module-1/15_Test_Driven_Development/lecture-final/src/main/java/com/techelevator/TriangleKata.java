97d1b7ccce15b2a2d34e096d11bdc2983a2d0de4
Brian <brian@techelevator.com>
2020-06-01 11:13:36 -0400

    extracted triangle logic

--- module-1/15_Test_Driven_Development/lecture-final/src/main/java/com/techelevator/TriangleKata.java
+++ module-1/15_Test_Driven_Development/lecture-final/src/main/java/com/techelevator/TriangleKata.java
@@ -9,18 +9,30 @@ public class TriangleKata {
 			return "invalid";
 		}
 	
-		if ((side1 == side2 && side1 != side3) ||
-				(side1 == side3 && side1 != side2) ||
-				(side2 == side3 && side1 != side2)) {
+		if (isIsosceles(side1, side2, side3)) {
 			return "isosceles";
 		}
 		
-		if( side1 == side2 && side1 == side3) {
+		if( isEquilateral(side1, side2, side3) ) {
 			return "equilateral";
 		}
 		
 		
 		return "scalene";
+	}
+	
+	
+	private boolean isEquilateral(int side1, int side2, int side3) {
+		return side1 == side2 && side1 == side3;
+	}
+	
+	private boolean isIsosceles(int side1, int side2, int side3) {
+		if ((side1 == side2 && side1 != side3) ||
+				(side1 == side3 && side1 != side2) ||
+				(side2 == side3 && side1 != side2)) {
+			return true;
+		}
+		return false;
 	}
 	
 	private boolean isValidTriangle(int side1, int side2, int side3) {
