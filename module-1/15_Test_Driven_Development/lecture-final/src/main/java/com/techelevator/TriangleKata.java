0b905af89e58e43fcbf038073015e3e079982159
Brian <brian@techelevator.com>
2020-06-01 10:44:45 -0400

    refactored invalid check

--- module-1/15_Test_Driven_Development/lecture-final/src/main/java/com/techelevator/TriangleKata.java
+++ module-1/15_Test_Driven_Development/lecture-final/src/main/java/com/techelevator/TriangleKata.java
@@ -4,17 +4,14 @@ public class TriangleKata {
 
 	public String classify(int side1, int side2, int side3) {
 		
-		if (side1 + side2 <= side3) {
+		if (side1 + side2 <= side3 ||
+				side2 + side3 <= side1 ||
+				side1 + side3 <= side2) {
 			return "invalid";
 		}
+	
 		
-		if (side2 + side3 <= side1) {
-			return "invalid";
-		}
-		
-		if (side1 + side3 <= side2) {
-			return "invalid";
-		}
+	
 		
 		
 		if( side1 == side2 && side1 == side3) {
