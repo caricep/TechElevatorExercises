d56654b4fbd1b73cf9429351d55c476e193e2a77
Brian <brian@techelevator.com>
2020-06-01 10:25:13 -0400

    setup project

---
+++ module-1/15_Test_Driven_Development/lecture-final/src/main/java/com/techelevator/TriangleKata.java
@@ -0,0 +1,11 @@
+package com.techelevator;
+
+public class TriangleKata {
+
+	public String classify(int side1, int side2, int side3) {
+		
+		
+		return null;
+	}
+	
+}
---
+++ module-1/15_Test_Driven_Development/lecture-final/src/test/java/com/techelevator/TriangleKataTest.java
@@ -0,0 +1,14 @@
+package com.techelevator;
+
+import org.junit.*;
+
+public class TriangleKataTest {
+
+	private TriangleKata triangleKata;
+	
+	@Before
+	public void setup() {
+		triangleKata = new TriangleKata();
+	}
+	
+}
