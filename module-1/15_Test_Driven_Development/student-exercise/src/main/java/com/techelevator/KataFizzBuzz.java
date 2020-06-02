fe2fac4a9edc84f4039f9f29259ca8a15d906321
Carice Poston <carice.cp@gmail.com>
2020-06-01 23:47:09 -0400

    Add passing code for test method result_is_1_when_number_is_1

--- module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -3,6 +3,10 @@ package com.techelevator;
 public class KataFizzBuzz {
 
 	public String fizzBuzz(int number) {
+		
+		if (number >= 1 || number <= 100) {
+			return "" + number + "";
+		}
 		return null;
 	}
 	
