d90bc162c4c665496b60c471cab9bb56e74ab67c
Brian Lauvray <lauvray.1@osu.edu>
2020-06-12 17:58:12 -0400

    Added completed Database Design Lecture addressbook schema

--- module-2/05_Database_Design/lecture-final/addressbook.sql
+++ module-2/05_Database_Design/lecture-final/addressbook.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS phone;
+DROP TABLE IF EXISTS email;
 DROP TABLE IF EXISTS person_address;
 DROP TABLE IF EXISTS person;
 DROP TABLE IF EXISTS address;
@@ -32,4 +34,35 @@ CREATE TABLE person_address (
         PRIMARY KEY(person_id, address_id),
         CONSTRAINT fk_person_person_id FOREIGN KEY (person_id) REFERENCES person(person_id),
         CONSTRAINT fk_address_address_id FOREIGN KEY (address_id) REFERENCES address(address_id)
+);
+
+CREATE TABLE phone (
+        phone_id serial primary key,
+        person_id int not null,
+        phone_number varchar(20) not null,
+        extension varchar(10),
+        phone_type varchar(10),
+        type_other_description varchar(32),
+        
+        CONSTRAINT fk_phone_person_id FOREIGN KEY (person_id) REFERENCES person(person_id),
+        CONSTRAINT chk_phone_type CHECK ( phone_type IN ('Work', 'Home', 'Mobile', 'Other') ),
+        
+        CONSTRAINT chk_phone_type_other CHECK ( (phone_type = 'Other' AND type_other_description IS NOT NULL) OR 
+                ( phone_type <> 'Other' AND type_other_description IS NULL)  ) 
+
+);
+
+CREATE TABLE email (
+        email_id serial primary key,
+        person_id int not null,
+        email varchar(255) not null,
+        email_type varchar(10),
+        type_other_description varchar(32),
+        
+        CONSTRAINT fk_email_person_id FOREIGN KEY (person_id) REFERENCES person(person_id),
+        CONSTRAINT chk_email_type CHECK ( email_type IN ('Work', 'Home','Other') ),
+        
+        CONSTRAINT chk_email_type_other CHECK ( (email_type = 'Other' AND type_other_description IS NOT NULL) OR 
+                ( email_type <> 'Other' AND type_other_description IS NULL)  ) 
+
 );
