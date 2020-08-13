3e47f0531baac544d8002b37c1845f75a8be191a
Carice Poston <carice.cp@gmail.com>
2020-08-13 19:50:49 -0400

    Module 4

---
+++ module-4/11_Vue_Component_Communication/src/components/ReadingList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="book-container">
+    <book-card
+    v-for="book in bookList" 
+    v-bind:key="book.id" v-bind:book='book'/>
+  </div>
+</template>
+
+<script>
+import BookCard from './BookCard';
+
+export default {
+  name: "reading-list",
+  components: {
+    BookCard
+  },
+  data() {
+      return {
+          bookList
+      }
+  },
+  computed: {
+      bookList() {
+          return this.$store.state.books;
+      }
+  }
+};
+</script>
+
+<style>
+.book-container {
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+}
+</style>
---
+++ module-4/13_Web_Services_GET/src/components/TopicList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="topic-list">
+    <router-link v-for="topic in topics" v-bind:key="topic.id" :to="{ name: 'Messages', params: {id: topic.id}}">
+    <div class="topic">
+      {{ topic.title }}
+    </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import TopicService from '../services/TopicService'
+
+export default {
+  name: 'topic-list',
+  data() {
+    return {
+      topics: []
+    }
+  },
+  created() {
+    TopicService.getTopics().then(response => {
+      this.topics = response.data;
+    });
+  }
+}
+</script>
+
+<style>
+.topic-list {
+  padding: 20px 20px;
+  margin: 0 auto;
+  max-width: 600px;
+}
+.topic {
+  font-size: 24px;
+  border-bottom: 1px solid #f2f2f2;
+  padding: 10px 20px;
+}
+.topic:last-child {
+  border: 0px;
+}
+</style>
